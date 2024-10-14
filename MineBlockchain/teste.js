d85fac3015a38b389eb8a8b607de1e8fd3fa23e1
rafael <flashinhorafael@gmail.com>
2024-10-13 22:05:32 -0300

    teste

--- MineBlockchain/teste.js
+++ MineBlockchain/teste.js
@@ -1,10 +1,10 @@
 const Blockchain = require('./blockchain');
 const Transaction = require('./transaction');
 
-const bitcoins = new Blockchain('primeiro endereço');
+const bitcoins = new Blockchain('primeiro endereço:');
 
-bitcoins.createTransaction(new Transaction('primeiro endereço', 'segundo endereço', 10)); 
-bitcoins.createTransaction(new Transaction('primeiro endereço', 'terceiro endereço', 10)); 
+bitcoins.createTransaction(new Transaction('primeiro endereço', 'segundo endereço', 20)); // teste para saber se está funcionando
+bitcoins.createTransaction(new Transaction('primeiro endereço', 'terceiro endereço', 40)); 
 
 bitcoins.minerarTraPendente('segundo endereço');
 
