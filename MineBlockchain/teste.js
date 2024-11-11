3eef1d9cf510bf2da963869147b42068b6a0bd62
rafael <flashinhorafael@gmail.com>
2024-11-10 21:49:49 -0300

    fixing

--- MineBlockchain/teste.js
+++ MineBlockchain/teste.js
@@ -16,9 +16,5 @@ console.log(`Essa blockchain é válida? ${bitcoins.validBlockchain()}`);
 const historico = bitcoins.historicTransaction('2x000000000000000000000000000000000000000001');
 console.log("Histórico de transações: ", historico);
 
-<<<<<<< HEAD
-=======
-console.log(bitcoins.validAddress("2x001abc245123"));  // teste da função retornar false
-console.log(bitcoins.validAddress("a1b2c3d4e5f67890123456789abcdef1234567890")); // retornar false
 
->>>>>>> f1cab22469f89a424c1a3f2b6473fb5c24d4ee3f
+
